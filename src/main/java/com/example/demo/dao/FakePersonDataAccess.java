15c88c3a1415e2030e48ba75c15043c032b26799
mark bashir <beejayphil@gmail.com>
2019-09-24 17:24:28 +0100

    added FakePersonData class

--- src/main/java/com/example/demo/dao/FakePersonDataAccess.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccess.java
@@ -1,4 +1,14 @@
 package com.example.demo.dao;
 
-public class FakePersonDataAccess {
+import com.example.demo.model.Person;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class FakePersonDataAccess implements PersonDao{
+    private static ArrayList<Person> person = new ArrayList();
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
 }
